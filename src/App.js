16b700c24c2f26d7585385c72e69f2b51c8cd567
Gagliarda <agus97gagliardetti@gmail.com>
2023-04-14 17:21:43 -0300

    Proyecto

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Cards from './components/Cards';
+import  Nav from './components/Nav'
+import { useState } from 'react';
+import axios from 'axios'
+
+
+
+
+
+function App() {
+   const [characters, setCharacters]= useState([]);
+
+   
+   const onSearch =(id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+   
+   const onClose = (id) =>{
+      const charactersFiltered = characters.filter(character =>
+         character.id !== Number(id))
+         setCharacters(charactersFiltered)
+      }
+      
+
+   
+
+ return (
+      <div className='App'>
+
+         <Nav onSearch={onSearch}/>
+         <Cards characters={characters} onClose={onClose} />
+         
+      </div>
+   );
+
+   }
+
+  
+
+
+export default App;
